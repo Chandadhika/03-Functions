abaeff2f9dd25e770d76277b08cbb1ae4e101ce7
ANarngAung <anarngaung2-17@gmail.com>
2024-05-18 13:52:53 +0630

    Created Functions and remove index file

---
+++ 03-Functions/Unsolved/script.js
@@ -0,0 +1,11 @@
+// TODO: Write Your JavaScript Code Here
+
+var evaluateEquality = function(value1, value2) {
+    if (value1 === value2) {
+        console.log("They are equal in type and value");
+    } else if (value1 == value2) {
+        console.log("They are equal in value");
+    } else {
+        console.log("The values are not equal");
+    }
+};
